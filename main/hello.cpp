130bf32a8c625c5e7b24360204a87f5a4dee7b8f
Mohith M Jogi <mohithjogi@gmail.com>
2025-03-13 16:22:16 +0530

    In Task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout<<"PES1UG22CS359\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<<"After making changes";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
